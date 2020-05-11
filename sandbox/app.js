5d299d7f604f6696e45cab647738891fbb337935
Charles Williams <Charles Williams>
2020-05-11 18:41:00 -0400

    added sandbox file

---
+++ sandbox/app.js
@@ -0,0 +1,165 @@
+// alert("Go get them");
+
+// const numbers = [1, 2, 3,4];
+// numbers.push(8,9);
+// console.log(numbers);
+// const name = 'John';
+// const age = 36;
+// const isMarried = false;
+
+// console.log(typeof name, age, isMarried);
+// const firstName = 'Charles';
+// const lastName = 'Williams';
+// let val;
+// val = firstName + ' ' + lastName;
+// val = 'Tina ';
+// val +=  'Williams';
+// val = 'What\'s up everybody, my name is' + ' ' + firstName + ' ' + lastName;
+// val = firstName.length;
+// val = firstName.charAt();
+// val = firstName.concat(' ', lastName);
+// val = lastName.concat(' ', firstName);
+// val = firstName.toLowerCase();
+// val = firstName.toUpperCase();
+// val = firstName.concat(' ', lastName);
+// val = firstName.length;
+// val = firstName[0];
+// val = firstName.indexOf('s');
+// val = lastName.lastIndexOf('l')
+// val = firstName.charAt(firstName.length-1)
+
+
+// console.log(val);
+// const name = 'Tina';
+// const age = 54;
+// const profession = ' educator';
+// const home = 'Charlotte';
+// let html;
+
+// html = '<ul>' +
+// '<li>name:' + name + '</li>' +
+// '<li>age: ' + age + '</li>' +
+// '<li>profession:' + profession + '</li>' +
+// '<li>home: ' + home + '</li>' + '</ul>';
+
+// document.body.innerHTML = html;
+
+// function goGo() {
+//     return 'DC';
+// }
+// // with template strings (es6) back ticks to the left of number 1 key
+
+// html = `
+// <ul>
+// <li>Name: ${name}</li>
+// <li>Name: ${age}</li>
+// <li>Name: ${profession}</li>
+// <li>Name: ${home}</li>
+// <li>Name: ${5+6}</li>
+// <li>Name: ${goGo()}</li>
+// </ul>;`
+
+
+// document.body.innerHTML = html;
+
+// // type conversion number to string
+// let val;
+// val = 5;
+// val = '5';
+// val = 'five';
+// val = String(new Date());
+// val = String(5);
+// val = String(true);
+// val = String([1,2,3,4,]);
+// val = new Date;
+// val = true;
+// val = 5;
+// // type conversion to a number
+// val = Number('78');
+// val = Number('number');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = parseInt(false);
+// val = parseInt(true);
+// val = Number(false);
+
+// // output
+// console.log(val);
+// console.log(typeof val);
+// // console.log(val.length);
+// const firstName = 'Charles';
+// const lastName = 'Williams';
+
+// let val;
+
+// // concatenation
+// val = firstName + ' ' + lastName;
+
+// // template literal
+
+// val = `${firstName} ${lastName}`;
+
+// // append
+// val = firstName;
+// val += lastName;
+
+// val = firstName.charAt(6);
+// val = firstName.charAt(firstName.length -1);
+
+// val = firstName.substring(0, 4);
+// console.log(val);
+
+
+
+// 
+// const firstName = 'Mark';
+// const lastName = 'Jackson';
+// const str = 'this is my passion';
+// let val;
+
+// val = firstName;
+// val += lastName;
+// val = lastName;
+// val = firstName.concat(' ', lastName);
+// val = lastName.charAt(lastName.length -1);
+// val = lastName.substring(0, 4);
+// val = str.split(' ');
+// val = firstName.slice(0, 4);
+
+
+// console.log(val);
+// const name = 'Charles';
+// const age = 52;
+// const job = 'coder';
+// const city = 'Charlotte';
+
+// let html;
+// function hello() {
+//     return 'hello';
+// }
+
+// html = `
+// <ul>
+// <li>name: ${name} </li>;
+// <li>age: ${age} </li>;
+// <li>job: ${job} </li>;
+// <li>city: ${city} </li>;
+// <li>${2+2}</li>;
+// <li>${hello()}</li>;
+// <li>${age > 52 ? 'you are geting up there' : 'you are a spring chicken'}</li>;
+// <li></li>;
+// </ul>
+
+
+// `;
+// document.body.innerHTML = html;
+
+
+
+
+
+
+
+
+
---
+++ sandbox/arrays.js
@@ -0,0 +1,79 @@
+// const numbers = [43, 56, 33, 23, 44, 36, 5];
+// const numbers2 = new Array(22, 45,33,76,54);
+// const fruit = ['Apple', 'Banana', 'orange', 'pear'];
+// const mixed = [22, 'hello', true, undefined, null, {a:1, b:1}, new Date() ];
+// let val;
+
+// val = fruit.sort();
+// val = numbers.sort();
+// val = numbers2.sort();
+
+// val = numbers.sort(function(x, y) {
+//     return y - x;
+// });
+
+// val = Math.floor(Math.random() * 10 + 1);
+// val = numbers.slice(0, 3);
+
+// console.log(val);
+// console.log(numbers);
+
+// const bike = 'mountain';
+// switch(bike) {
+//     case 'tenSpeed':
+//         console.log('This is a fast bike');
+//         break;
+//     case 'mountain':
+//         console.log('This is a powerful bike');
+//         break;
+//     default:
+//         console.log('This is not a fast bike and this is not a powerful bike');
+//         break;
+// }
+
+// let day;
+
+// switch(new Date().getDay()) {
+//     case 0:
+//         day = 'Sunday';
+//         break;
+//      case 1:
+//          day = ' Monday';
+//          break;
+//     case 2:
+//         day = 'Tuesday';
+//         break;
+//     case 3:
+//         day = 'Wednesday';
+//         break;
+//      case 4:
+//         day = 'Thursday';
+//         break;
+//     case 5:
+//         day = 'Friday';
+//         break;
+//     case 6:
+//          day = 'Saturday';
+//         break;
+                             
+// }
+
+// console.log(`Today is ${day}`);
+
+// function picColor(color) {
+//     return 'This color is my favorite' + ' ' + color;
+// }
+// console.log(picColor('green'));
+
+
+for(let i = 0; i <= 10; i++){
+    if (i === 2) {
+    console.log('This is my favorite number' + i);
+}
+
+else {console.log('This is not my favorite Number ' + i)};
+}
+Val =
+Math.floor(Math.random() * 50 + 1);
+
+console.log(Val);
